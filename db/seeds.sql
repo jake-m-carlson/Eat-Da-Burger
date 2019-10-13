adff35eb6ba9707e99c9dda1e04a1564babb40f8
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-10-13 16:04:46 -0500

    set up mysql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devour) VALUES ('Turkey Burger', FALSE);
+INSERT INTO burgers (burger_name, devour) VALUES ('Guacamole Burger', FALSE);
