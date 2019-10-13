e7b819015a4c8c5131ae3105bd7cb35ed8453af4
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-10-13 18:05:42 -0500

    server connected, set up intital hndlbrs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req,res){
-    burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render("index");
-    })
+    // burger.all(function(burger_data){
+    //     console.log(burger_data);
+    //     res.render("index");
+    // })
+    res.render("index"); // remove this later on and unmark 3 lines above
+
 })
 
 module.exports = router;
