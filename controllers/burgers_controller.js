d06342e5f8e4af58c5ed80afbb98867b0a2d9379
Jake Carlson <jakecarlson@jakes-mbp.lan>
2019-10-13 17:10:34 -0500

    imported express, burger.js, made router, exported

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req,res){
+    burger.all(function(burger_data){
+        console.log(burger_data);
+        res.render("index");
+    })
+})
+
+module.exports = router;
