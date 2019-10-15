8a4504a19d42bd0faa3a163268bd13d9a4091c58
Jake Carlson <jakecarlson@stp-er-10-129-31-224.wireless.umn.edu>
2019-10-15 18:29:04 -0500

    attempt to fix heroku

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"))
+// app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -23,4 +24,8 @@ app.set("view engine","handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use("/",routes);
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log("Listening on PORT " + PORT);
+    });
+    
+// app.listen(PORT);
