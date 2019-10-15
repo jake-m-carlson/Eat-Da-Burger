b6dbb6f8584b689d744cf08ae04591ad528567c4
Jake Carlson <jakecarlson@stp-er-10-129-31-224.wireless.umn.edu>
2019-10-15 18:33:44 -0500

    attempting server fix

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 app.use(express.static("public"))
+app.use(express.json());
 // app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
