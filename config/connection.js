a32ba9c364473931143f6d34936d9212b73295b5
Jake Carlson <jakecarlson@stp-er-10-129-31-224.wireless.umn.edu>
2019-10-15 21:45:03 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Modcoz16!",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Modcoz16!',
+        database: 'burgers_db'
+    })
+};
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Modcoz16!",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
